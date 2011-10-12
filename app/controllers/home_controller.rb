bc22d80214d0ff0c3581c4769e502404af552432
Vikko <vikkosmi@hotmail.com>
2011-10-12 16:03:47 +0200

    mailer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
-load_and_authorize_resource :about, :case, :blogpage
   def index
     @sidetext = About.find(6)
     @about = About.first
-    @cases = Case.find(:all, :order => "created_at DESC", :limit => 3)
-    @blogpages = Blogpage.find(:all, :order => "created_at DESC", :limit => 3)
+    @cases = Case.find(:all, :order => "created_at DESC", :limit => 3, :conditions => ["published = ?", true])
+    @blogpages = Blogpage.find(:all, :order => "created_at DESC", :limit => 3, :conditions => ["published = ?", true])
   end
 end
