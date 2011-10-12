bce388b8282d74bf7909beb92336e07d4ff48b85
Vikko <vikkosmi@hotmail.com>
2011-10-12 14:02:11 +0200

    Added about + sidetext

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 load_and_authorize_resource :about, :case, :blogpage
   def index
+    @sidetext = About.find(6)
     @about = About.first
     @cases = Case.find(:all, :order => "created_at DESC", :limit => 3)
     @blogpages = Blogpage.find(:all, :order => "created_at DESC", :limit => 3)
