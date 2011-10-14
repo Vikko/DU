f61330d190e1a96581a7aa07d247e16179c697e0
Vikko <vikkosmi@hotmail.com>
2011-10-14 16:17:48 +0200

    homepage about.find(1)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @sidetext = About.find(6)
-    @about = About.first
+    @about = About.find(1)
     @cases = Case.where(:published => true).order("created_at DESC").limit(3)
     @blogpages = Blogpage.where(:published => true).order("created_at DESC").limit(3)
   end
