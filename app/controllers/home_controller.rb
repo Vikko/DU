c514eb78825e21013c5edc474d34751e3173eda9
Vikko <vikkosmi@hotmail.com>
2011-10-19 12:13:14 +0200

    removed about text

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
     @sidetext = About.find(6)
-    @about = About.find(1)
     @cases = Case.where(:published => true).order("created_at DESC").limit(3)
     @blogpages = Blogpage.where(:published => true).order("created_at DESC").limit(3)
     @agenda = Event.find(:all, :order => "date ASC", :limit => 4)
