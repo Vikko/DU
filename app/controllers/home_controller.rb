20944f4727ba7c5fb5bb1b0b5da0ae98f535b42d
Vikko <vikkosmi@hotmail.com>
2011-10-19 11:35:55 +0200

    cleared up the forms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
     @about = About.find(1)
     @cases = Case.where(:published => true).order("created_at DESC").limit(3)
     @blogpages = Blogpage.where(:published => true).order("created_at DESC").limit(3)
+    @agenda = Event.find(:all, :order => "date ASC", :limit => 4)
   end
 end
