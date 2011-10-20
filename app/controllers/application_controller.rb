ac16473879bc865fce3274372ee52b1343090882
Vikko <vikkosmi@hotmail.com>
2011-10-20 11:01:26 +0200

    Related events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,36 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def load_related(page)
+    search = []
+    unless page.tags.blank?
+      search = page.tags.split(", ")
+    end
+    pages = []
+    profiles = []
+    search.each do |s|
+      pages << Page.where("tags LIKE ?", '%'+s+'%')
+      profiles << Profile.where("tags LIKE ?", '%'+s+'%')
+    end
+    @relatedpages = []
+    @relatedevents = []
+    @relatedprofiles = []
+    pages.each do |pa|
+      pa.each do |page|
+        if page.type == "Event"
+          @relatedevents << page
+        else
+          @relatedpages << page
+        end
+      end
+    end
+    profiles.each do |pr|
+      pr.each do |profile|
+        @relatedprofiles << profile
+      end 
+    end
+    @relatedpages.uniq!
+    @relatedevents.uniq!
+    @relatedprofiles.uniq!
+    end
 end
--- app/controllers/blogpages_controller.rb
+++ app/controllers/blogpages_controller.rb
@@ -7,30 +7,8 @@ class BlogpagesController < InheritedResources::Base
   def show
     @blogpage = Blogpage.find(params[:id])
     @comments = @blogpage.comments
-    search = []
-    if @blogpage.tags
-      search = @blogpage.tags.split(", ")
-    end
-    pages = []
-    profiles = []
-    search.each do |s|
-      pages << Page.where("tags LIKE ?", '%'+s+'%')
-      profiles << Profile.where("tags LIKE ?", '%'+s+'%')
-    end
-    @relatedpages = []
-    @relatedprofiles = []
-    pages.each do |pa|
-      pa.each do |page|
-        @relatedpages << page
-      end
-    end
-    profiles.each do |pr|
-      pr.each do |profile|
-        @relatedprofiles << profile
-      end 
-    end
-    @relatedpages.uniq!
-    @relatedprofiles.uniq!
+    load_related(@blogpage)
+    @agenda = @relatedevents
   end
   
   def create
