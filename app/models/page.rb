2bcdc537e7f63a38289725c932af5d673925823e
vikko <vikkosmi@hotmail.com>
2013-03-08 21:42:19 +0100

    scoped for archive

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,7 @@
 class Page < ActiveRecord::Base
   scope :random, lambda { |*args| { :order=>'RAND()', :limit=>args[0] || 1 } }
+  scope :archived, where("archive = 1")
+  scope :not_archived, where("archive = 0")
   
   @regex = /^[a-zA-Z0-9\-_]*$/
   
